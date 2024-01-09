58dff22a314e6b7ecbbb78522749bc543c8effc5
Matthew D <42253757+Resident-Fortress@users.noreply.github.com>
2024-01-09 14:02:39 -0600

    Add files via upload

---
+++ Source1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+void doPrint(
+	std::cout << "This is an example of void function in C++" << std::endl
+)
+
+int main() 
+{
+	doPrint()
+	std::cout << "I'm bored here honestly. I want to learn more"
+}
